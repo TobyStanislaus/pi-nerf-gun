2df48e810836a8c983cb5f9ec9d4e14f1c363d02
Toby <toby@stanislaus.co.uk>
2025-03-13 20:57:49 +0000

    initial push - sped up system greatly

--- servo_control.py
+++ servo_control.py
@@ -6,7 +6,7 @@ def set_angle(angle, servo_pin, pi):
     """Moves the servo to a specific angle using pigpio."""
     pulse_width = int(500 + (angle / 180.0) * 2000)  # Convert angle to PWM pulse width
     pi.set_servo_pulsewidth(servo_pin, pulse_width)
-    sleep(0.5)
+    sleep(0.1)
 
 def pull_switch(servo_pin, pi):
     """Performs the switch pull sequence."""
